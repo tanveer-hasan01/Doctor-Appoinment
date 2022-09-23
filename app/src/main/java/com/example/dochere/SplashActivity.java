f2279ff66f85ac6c9636a68940adaaddabcd6575
tanveerhasan <tanveer.cse01@gmail.com>
2022-09-23 16:37:06 +0600

    init comit

---
+++ app/src/main/java/com/example/dochere/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.dochere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 3;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, secondsDelayed * 1000);
+    }
+}
