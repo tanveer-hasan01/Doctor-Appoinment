34ed13ea89b51e502ba32031936c405906b9ed29
tanveerhasan <tanveer.cse01@gmail.com>
2022-09-25 21:08:51 +0600

    ui design

--- app/src/main/java/com/example/dochere/SplashActivity.java
+++ app/src/main/java/com/example/dochere/SplashActivity.java
@@ -8,16 +8,28 @@ import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
+    MysharedPreferance mysharedPreferance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        mysharedPreferance = MysharedPreferance.getPreferences(getApplicationContext());
         int secondsDelayed = 3;
         new Handler().postDelayed(new Runnable() {
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+
+                if (mysharedPreferance.getlogin_type().equals("patient")){
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    finish();
+                }else if(mysharedPreferance.getlogin_type().equals("doc")) {
+                    startActivity(new Intent(SplashActivity.this, DoctorLoginActivity.class));
+                    finish();
+                }else{
+                    startActivity(new Intent(SplashActivity.this, AppMoodActivity.class));
+                    finish();
+                }
+
             }
         }, secondsDelayed * 1000);
     }
