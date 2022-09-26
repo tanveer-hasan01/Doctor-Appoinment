c9cfcd7912479e3444b0f9e8831386c5d06b0c78
tanveerhasan <tanveer.cse01@gmail.com>
2022-09-26 16:58:58 +0600

    added fragment

--- app/src/main/java/com/example/dochere/AppMoodActivity.java
+++ app/src/main/java/com/example/dochere/AppMoodActivity.java
@@ -2,13 +2,36 @@ package com.example.dochere;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.dochere.databinding.ActivityAppMoodBinding;
 
 public class AppMoodActivity extends AppCompatActivity {
+
+    ActivityAppMoodBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_mood);
+        binding = ActivityAppMoodBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.patient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AppMoodActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        binding.doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AppMoodActivity.this, DoctorLoginActivity.class));
+                finish();
+            }
+        });
     }
 }
