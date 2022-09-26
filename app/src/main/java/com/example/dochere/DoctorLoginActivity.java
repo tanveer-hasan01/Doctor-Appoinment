5c69067d4f2463c9cb6baf13f3b47ce30ac23c45
tanveerhasan <tanveer.cse01@gmail.com>
2022-09-26 21:50:03 +0600

    fragment design

--- app/src/main/java/com/example/dochere/DoctorLoginActivity.java
+++ app/src/main/java/com/example/dochere/DoctorLoginActivity.java
@@ -10,5 +10,6 @@ public class DoctorLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_login);
+        setTitle("Doctor Login");
     }
 }
