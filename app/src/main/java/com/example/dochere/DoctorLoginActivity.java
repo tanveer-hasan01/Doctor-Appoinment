56ea885301de72fd1ebab3cf285e4b603147eaff
tanveerhasan <tanveer.cse01@gmail.com>
2022-10-05 01:11:09 +0600

    added some modification by tanveer

--- app/src/main/java/com/example/dochere/DoctorLoginActivity.java
+++ app/src/main/java/com/example/dochere/DoctorLoginActivity.java
@@ -3,13 +3,43 @@ package com.example.dochere;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.dochere.databinding.ActivityDoctorDatilesBinding;
+import com.example.dochere.databinding.ActivityDoctorLoginBinding;
+import com.example.dochere.network.ApiClient;
+import com.example.dochere.network.ApiInterface;
+
+import retrofit2.Retrofit;
 
 public class DoctorLoginActivity extends AppCompatActivity {
+
+    ApiInterface apiInterface;
+    MysharedPreferance mysharedPreferance;
+    ActivityDoctorLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_doctor_login);
+        binding = ActivityDoctorLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setTitle("Doctor Login");
+
+        Retrofit instance = ApiClient.instance();
+        apiInterface = instance.create(ApiInterface.class);
+        mysharedPreferance = MysharedPreferance.getPreferences(this);
+
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (!binding.email.getText().toString().isEmpty()|| !binding.password.getText().toString().isEmpty()){
+
+
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dochere/adapter/AdapterMedicine.java
+++ app/src/main/java/com/example/dochere/adapter/AdapterMedicine.java
@@ -1,4 +1,46 @@
 package com.example.dochere.adapter;
 
-public class AdapterMedicine {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dochere.model.ModelMedicine;
+
+import java.util.ArrayList;
+
+public class AdapterMedicine extends RecyclerView.Adapter<AdapterMedicine.Holder> {
+
+
+    ArrayList<ModelMedicine>medicines;
+    Context context;
+
+    public AdapterMedicine(ArrayList<ModelMedicine> medicines, Context context) {
+        this.medicines = medicines;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public AdapterMedicine.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterMedicine.Holder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
