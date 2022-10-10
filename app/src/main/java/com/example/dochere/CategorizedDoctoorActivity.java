015a165d9453b8cc210c61f556df8f9338b98940
tanveerhasan <tanveer.cse01@gmail.com>
2022-10-10 19:48:07 +0600

    added some modification by tanveer

--- app/src/main/java/com/example/dochere/CategorizedDoctoorActivity.java
+++ app/src/main/java/com/example/dochere/CategorizedDoctoorActivity.java
@@ -1,6 +1,7 @@
 package com.example.dochere;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import com.example.dochere.adapter.AdapterDoc;
 import com.example.dochere.databinding.ActivityCategorizedDoctoorBinding;
 import com.example.dochere.databinding.ActivityEditProfileBinding;
 import com.example.dochere.model.ModelDoc;
+import com.example.dochere.network.ApiClient;
 import com.example.dochere.network.ApiInterface;
 
 import java.util.ArrayList;
@@ -18,6 +20,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class CategorizedDoctoorActivity extends AppCompatActivity {
 
@@ -33,14 +36,17 @@ public class CategorizedDoctoorActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         setTitle(getIntent().getStringExtra("category"));
 
+        Retrofit instance = ApiClient.instance();
+        apiInterface = instance.create(ApiInterface.class);
 
 
-
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(CategorizedDoctoorActivity.this,LinearLayoutManager.VERTICAL,false));
+        adapterDoc = new AdapterDoc(docs,CategorizedDoctoorActivity.this);
 
         ModelDoc modelDoc = new ModelDoc();
         modelDoc.setCategory(getIntent().getStringExtra("category"));
         ProgressDialog dialog1 = ProgressDialog.show(CategorizedDoctoorActivity.this, "Searching...", "Please wait...", true);
-        apiInterface.searchDoc(modelDoc).enqueue(new Callback<List<ModelDoc>>() {
+        apiInterface.categorized_doc(modelDoc).enqueue(new Callback<List<ModelDoc>>() {
             @Override
             public void onResponse(Call<List<ModelDoc>> call, Response<List<ModelDoc>> response) {
                 dialog1.dismiss();
@@ -52,6 +58,7 @@ public class CategorizedDoctoorActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<ModelDoc>> call, Throwable t) {
+                dialog1.dismiss();
                 Toast.makeText(CategorizedDoctoorActivity.this, "Doctor not found", Toast.LENGTH_SHORT).show();
             }
         });
