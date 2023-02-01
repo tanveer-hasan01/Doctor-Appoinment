47c26eb8d3d7f52d669353a0f475f0a912394c76
tanveerhasan <tanveer.cse01@gmail.com>
2023-02-01 15:41:19 +0600

    added some modification by tanveer

--- app/src/main/java/com/example/dochere/adapter/AdapterAppoitment.java
+++ app/src/main/java/com/example/dochere/adapter/AdapterAppoitment.java
@@ -1,31 +1,61 @@
 package com.example.dochere.adapter;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dochere.BuildConfig;
 import com.example.dochere.R;
 import com.example.dochere.RatingActivity;
 import com.example.dochere.model.ModelAppoitment;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class AdapterAppoitment extends RecyclerView.Adapter<AdapterAppoitment.Holder> {
 
+    private static final String TAG ="dff" ;
     ArrayList<ModelAppoitment>appoitments;
     Context context;
+     AlertDialog dialog_condition;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
 
     public AdapterAppoitment(ArrayList<ModelAppoitment> appoitments, Context context) {
         this.appoitments = appoitments;
@@ -41,6 +71,9 @@ public class AdapterAppoitment extends RecyclerView.Adapter<AdapterAppoitment.Ho
 
     @Override
     public void onBindViewHolder(@NonNull AdapterAppoitment.Holder holder, @SuppressLint("RecyclerView") int position) {
+
+
+        hasPermission();
 
         holder.name.setText(appoitments.get(position).getName());
         holder.docname.setText(appoitments.get(position).getDocName());
@@ -67,24 +100,50 @@ public class AdapterAppoitment extends RecyclerView.Adapter<AdapterAppoitment.Ho
             @Override
             public void onClick(View view) {
 
+                ConstraintLayout constraintLayout;
                 EditText prescription,food;
+                TextView p_name,d_name,p_age,date;
                 Button submit;
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 final View mview = LayoutInflater.from(context).inflate(R.layout.write_prescription, null);
                 builder.setView(mview);
-                final AlertDialog dialog_condition = builder.create();
+                dialog_condition = builder.create();
                 // dialog_condition.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
                 dialog_condition.show();
 
                 prescription = mview.findViewById(R.id.et_prescription);
                 food = mview.findViewById(R.id.food);
+                constraintLayout = mview.findViewById(R.id.layout);
+                p_age = mview.findViewById(R.id.p_age);
+                p_name = mview.findViewById(R.id.p_name);
+                date = mview.findViewById(R.id.date);
+                d_name = mview.findViewById(R.id.doc_name);
                 submit = mview.findViewById(R.id.submit);
-                submit.setVisibility(View.GONE);
+                submit.setText("Download");
+                submit.setVisibility(View.VISIBLE);
                 prescription.setEnabled(false);
                 food.setEnabled(false);
+                p_name.setVisibility(View.VISIBLE);
+                p_age.setVisibility(View.VISIBLE);
+                d_name.setVisibility(View.VISIBLE);
+                date.setVisibility(View.VISIBLE);
+
+                p_name.setText("Patient: "+appoitments.get(position).getName());
+                p_age.setText("Age: "+appoitments.get(position).getAge());
+                d_name.setText("Doc : "+appoitments.get(position).getDocName());
+                date.setText("Date : "+appoitments.get(position).getDate());
                 food.setText(appoitments.get(position).getFood());
                 prescription.setText(appoitments.get(position).getPrescription());
+
+                submit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        submit.setVisibility(View.GONE);
+                        getBitmapFromView(constraintLayout,appoitments.get(position).getId());
+
+                    }
+                });
             }
         });
 
@@ -138,4 +197,80 @@ public class AdapterAppoitment extends RecyclerView.Adapter<AdapterAppoitment.Ho
 
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public Bitmap getBitmapFromView(View view, String f_id) {
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.draw(canvas);
+        bitmapToPdf(bitmap, f_id);
+        return bitmap;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void bitmapToPdf(Bitmap bitmap, String f_id) {
+
+        PdfDocument pdfDocument = new PdfDocument();
+        PdfDocument.PageInfo pi = new PdfDocument.PageInfo.Builder(bitmap.getWidth(), bitmap.getHeight(), 1).create();
+        PdfDocument.Page page = pdfDocument.startPage(pi);
+        Canvas canvas = page.getCanvas();
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#FFFFFF"));
+        canvas.drawPaint(paint);
+        bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+        paint.setColor(Color.BLUE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        pdfDocument.finishPage(page);
+
+        File root = new File(Environment.getExternalStorageDirectory(), "Download");
+        if (!root.exists()) {
+            root.mkdir();
+        }
+        File file = new File(root, f_id + ".pdf");
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            try {
+                pdfDocument.writeTo(fileOutputStream);
+                Toast.makeText(context, "ডাউনলোড সফল, মোবাইলের Download ফোল্ডের চেক করুন ", Toast.LENGTH_SHORT).show();
+
+            } catch (IOException e) {
+                Log.d(TAG, "bitmapToPdf1: "+e.getMessage());
+                e.printStackTrace();
+
+            }
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "bitmapToPdf2: "+e.getMessage());
+            e.printStackTrace();
+        }
+        pdfDocument.close();
+
+
+
+        dialog_condition.dismiss();
+
+        //open pdf
+      /*  Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".fileprovider", file);
+        intent.setDataAndType(uri, "application/pdf");
+        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        context.startActivity(Intent.createChooser(intent, "Open with..."));*/
+
+    }
+
+
+    public void hasPermission() {
+        int permission = ActivityCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    (Activity) context,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
 }
+
